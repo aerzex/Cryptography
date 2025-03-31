ddbd99fee078f2419919eb3af48f9ae123233857
aerzex <aerzex@bk.ru>
2025-04-01 00:47:58 +1000

    Fix Number Factorization script
    small fix in wrong path to lib in pollard p-1 script

--- Math Algorithms/Numbers Factorization/pollard_p_minus_1.py
+++ Math Algorithms/Numbers Factorization/pollard_p_minus_1.py
@@ -4,7 +4,7 @@ import os
 import math
 import json
 
-lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '.', 'algorithms'))
+lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'algorithms'))
 sys.path.append(lib_path)
 from algorithms import algorithm_fast_pow, algorithm_euclid_extended, algorithm_Miller_Rabin_test
 
