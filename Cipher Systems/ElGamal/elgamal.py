00122f45cba0826d041e2cee609d513bc760b8b1
aerzex <aerzex@bk.ru>
2025-04-03 15:12:39 +1000

    Update elgamal.py

--- Cipher Systems/ElGamal/elgamal.py
+++ Cipher Systems/ElGamal/elgamal.py
@@ -99,10 +99,10 @@ def decrypt(enc_message):
     return b''.join(dec_blocks).decode('utf-8')
 
 def main():
-    size = int(input("Enter size of N: "))
+    #size = int(input("Enter size of N: "))
     message_en = "If I don’t like a thing, I don’t like it, that’s all; and there is no reason under the sun why I should ape a liking for it just because the majority of my fellow-creatures like it, or make believe they like it. I can’t follow the fashions in the things I like or dislike."
     message_ru = "Если мне что-то не нравится, значит, не нравится, и все тут; так с какой стати, спрашивается, я стану делать вид, будто мне это нравится, только потому, что большинству моих соплеменников это нравится или они воображают, что нравится. Не могу я что-то любить или не любить по велению моды."
-    generate_keys(size)
+    #generate_keys(size)
     enc_message = encrypt(message_ru)
     dec_message = decrypt(enc_message)
     print(enc_message)
